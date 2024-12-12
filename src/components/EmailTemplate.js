98f147f2c55ab9fb305b20fad8e5a72636a51bb4
fthrobby <rbysalam10@gmail.com>
2024-12-12 11:58:18 +0700

    🐛 fix: create new email recieved template

--- src/components/EmailTemplate.js
+++ src/components/EmailTemplate.js
@@ -1,9 +1,108 @@
-const EmailTemplate = ({ firstName }) => {
-  return (
-    <div>
-      <h1>Test, Welcome {firstName}</h1>
-    </div>
-  );
-};
+import {
+  Body,
+  Button,
+  Container,
+  Head,
+  Hr,
+  Html,
+  Img,
+  Preview,
+  Section,
+  Text,
+} from "@react-email/components";
+import React from "react";
+
+export const EmailTemplate = ({ body, email, nama }) => (
+  <Html>
+    <Head />
+    <Preview>
+      The sales intelligence platform that helps you uncover qualified leads.
+    </Preview>
+    <Body style={main}>
+      <Container style={container}>
+        <Img
+          src={`https://www.frobby.tech/logo.jpg`}
+          width="170"
+          height="170"
+          alt="logo"
+          style={logo}
+        />
+        <Text style={paragraph}>
+          Hi Fatah, you've new message from your page
+        </Text>
+        <Text style={paragraph}>{body}</Text>
+        <Text style={paragraph}>
+          This message from : {nama} | {email}
+        </Text>
+        <Section style={btnContainer}>
+          <Button
+            href={`mailto:${email}`}
+            style={{
+              width: "100%",
+              boxSizing: "border-box",
+              padding: 12,
+              fontWeight: 600,
+              borderRadius: 8,
+              textAlign: "center",
+              backgroundColor: "#1b1b1b",
+              color: "#f5f5f5",
+            }}
+          >
+            Reply Message
+          </Button>
+        </Section>
+        <Hr style={hr} />
+        <Text style={footer}>
+          Auto message forwarding, made by Fatah RobbySalam
+        </Text>
+      </Container>
+    </Body>
+  </Html>
+);
 
 export default EmailTemplate;
+
+const main = {
+  backgroundColor: "#ffffff",
+  fontFamily:
+    '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", sans-serif',
+};
+
+const container = {
+  margin: "0 auto",
+  padding: "20px 0 48px",
+};
+
+const logo = {
+  margin: "0 auto",
+};
+
+const paragraph = {
+  fontSize: "16px",
+  lineHeight: "26px",
+};
+
+const btnContainer = {
+  textAlign: "center",
+};
+
+const button = {
+  backgroundColor: "#1b1b1b",
+  borderRadius: "3px",
+  color: "#fff",
+  fontSize: "16px",
+  textDecoration: "none",
+  textAlign: "center",
+  display: "block",
+  padding: "12px",
+};
+
+const hr = {
+  borderColor: "#cccccc",
+  margin: "20px 0",
+};
+
+const footer = {
+  color: "#8898aa",
+  fontSize: "12px",
+};
