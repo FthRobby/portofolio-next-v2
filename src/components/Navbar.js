e22ee8309415d84e76be4e846d0aa4b522084f27
fthrobby <rbysalam10@gmail.com>
2024-12-12 14:09:25 +0700

    🔥 update: fix navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -234,7 +234,7 @@ const Navbar = () => {
             <motion.a
               target={"_blank"}
               className="w-6 mr-3"
-              href="https://github.com/lilxyzz"
+              href="https://github.com/fthrobby"
               whileHover={{ y: -2 }}
               whileTap={{ scale: 0.9 }}
               aria-label="Checkout my Github profile"
