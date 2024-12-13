99057f6c32a04f6badfddf9471ece7140bfaa3ae
fthrobby <rbysalam10@gmail.com>
2024-12-13 11:25:21 +0700

    🐛 fix: fixed some api components

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,5 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  if (req.method === "GET") {
+    const response =
+      "Hello! What’s going on? Are you looking to create something interesting or collaborate? Feel free to contact me on the contact page, and I’ll get back to you as soon as possible. See yaa!";
+    res.status(200).json(response);
+  } else {
+    res.setHeader("Allow", ["GET"]);
+    return res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
 }
