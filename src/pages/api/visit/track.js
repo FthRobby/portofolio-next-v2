4309cf16b7709f6e35775981f91fbe1b5e34b172
fthrobby <rbysalam10@gmail.com>
2024-12-17 13:40:39 +0700

    🐛 update: test update in prodution

--- src/pages/api/visit/track.js
+++ src/pages/api/visit/track.js
@@ -4,16 +4,18 @@ import { getUserIp } from "@/lib/getUser";
 export default async function handler(req, res) {
   if (req.method === "POST") {
     const { page, userAgent } = req.body;
-    // Get the user's IP address from the x-forwarded-for header
-    const userIp = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
-
-    const response = await fetch(
-      `https://ipinfo.io/${userIp}?token${process.env.NEXT_PUBLIC_IPTOKEN}`
-    );
-    const dataIp = await response.json();
-
+    const userIp =
+      req.headers["x-forwarded-for"] || req.connection.remoteAddress;
     try {
-      // Insert the visit record into the database
+      const response = await fetch(
+        `https://ipinfo.io/${userIp}?token${process.env.NEXT_PUBLIC_IPTOKEN}`
+      );
+
+      if (!response.ok) {
+        throw new Error("failed fetch data");
+      }      
+      const dataIp = await response.json();
+
       const { data, error } = await supabase.from("page_visits").insert([
         {
           page,
@@ -27,7 +29,7 @@ export default async function handler(req, res) {
         return res.status(500).json({ error: error.message });
       }
 
-      return res.status(200).json({ message: "Track successfully", dataIp });
+      return res.status(200).json({ message: "Track successfully" });
     } catch (error) {
       return res.status(500).json({ error: error.message });
     }
