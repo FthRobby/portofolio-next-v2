7e5798982170846f0e04835f08076e3d6dbdc9ed
fthrobby <rbysalam10@gmail.com>
2024-12-19 11:50:59 +0700

    🔥 update: test functions

--- src/pages/api/visit/get.js
+++ src/pages/api/visit/get.js
@@ -3,32 +3,51 @@ import supabase from "@/lib/supabase";
 export default async function handler(req, res) {
   if (req.method === "POST") {
     const { page, userAgent } = req.body;
-    const userIp = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
-
-
-
+    const userIp =
+      req.headers["x-forwarded-for"] || req.connection.remoteAddress;
     try {
-      const {data, error} = await supabase
+      // Perform the upsert operation
+      const { data, error } = await supabase
         .from("page_visits")
-        .insert([
-          {
-            page,
-            user_ip: userIp,
-            user_agent: userAgent,
-            timestamp: new Date()
-          }
-        ])
+        .upsert(
+          [
+            {
+              page,
+              user_ip: userIp,
+              user_agent: userAgent,
+              total_visit: 1,
+              timestamp: new Date(),
+            },
+          ],
+          { onConflict: ["user_ip"] }
+        )
+        .select();
       if (error) {
         throw new Error(error.message);
       }
+      const existingRecord = data.find((row) => row.user_ip === userIp);
+      if (existingRecord) {
+        const { error: updateError } = await supabase
+          .from("page_visits")
+          .update({ total_visit: existingRecord.total_visit + 1 })
+          .eq("user_ip", userIp);
 
+        if (updateError) {
+          throw new Error(updateError.message);
+        }
+      }
+
+      // Respond with success
       return res.status(200).json({
         status: "success",
-        data
+        data,
       });
-    } catch (error) {
+    } catch (err) {
+      // Catch and handle errors
+      console.error("Error:", err.message);
       return res.status(500).json({
-        error: error.message,
+        status: "error",
+        message: err.message,
       });
     }
   } else {
@@ -37,4 +56,3 @@ export default async function handler(req, res) {
     });
   }
 }
-
