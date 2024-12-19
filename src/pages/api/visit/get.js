47b819c0770ed869c0df49bf835ed8a6fbb56c88
fthrobby <rbysalam10@gmail.com>
2024-12-19 12:12:20 +0700

    🔥 update: update functions

--- src/pages/api/visit/get.js
+++ src/pages/api/visit/get.js
@@ -6,44 +6,54 @@ export default async function handler(req, res) {
     const userIp =
       req.headers["x-forwarded-for"] || req.connection.remoteAddress;
     try {
-      // Perform the upsert operation
-      const { data, error } = await supabase
+      const { data: existingRecord, error: selectError } = await supabase
         .from("page_visits")
-        .upsert(
-          [
-            {
-              page,
-              user_ip: userIp,
-              user_agent: userAgent,
-              total_visit: 1,
-              timestamp: new Date(),
-            },
-          ],
-          { onConflict: ["user_ip"] }
-        )
-        .select();
-      if (error) {
-        throw new Error(error.message);
+        .select()
+        .eq("user_ip", userIp);
+
+      if (selectError) {
+        throw new Error(selectError.message);
       }
-      const existingRecord = data.find((row) => row.user_ip === userIp);
-      if (existingRecord) {
+
+      if (existingRecord && existingRecord.length > 0) {
         const { error: updateError } = await supabase
           .from("page_visits")
-          .update({ total_visit: existingRecord.total_visit + 1 })
+          .update({
+            total_visit: existingRecord[0].total_visit + 1,
+            user_agent: userAgent,
+            timestamp: new Date(),
+          })
           .eq("user_ip", userIp);
 
         if (updateError) {
           throw new Error(updateError.message);
         }
-      }
 
-      // Respond with success
-      return res.status(200).json({
-        status: "success",
-        data,
-      });
+        return res.status(200).json({
+          status: "success",
+          message: "Visit count updated",
+        });
+      } else {
+        const { error: insertError } = await supabase
+          .from("page_visits")
+          .insert({
+            page,
+            user_ip: userIp,
+            user_agent: userAgent,
+            total_visit: 1,
+            timestamp: new Date(),
+          });
+
+        if (insertError) {
+          throw new Error(insertError.message);
+        }
+
+        return res.status(200).json({
+          status: "success",
+          message: "New visit recorded",
+        });
+      }
     } catch (err) {
-      // Catch and handle errors
       console.error("Error:", err.message);
       return res.status(500).json({
         status: "error",
