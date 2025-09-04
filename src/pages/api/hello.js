0864f24455b85d58901cdb15207f07a4d7bda7d8
FthRobby <rbysalam10@gmail.com>
2025-09-04 15:32:58 +0700

    Add maintenance page

--- src/pages/api/hello.js
+++
@@ -1,6 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-export default function handler(res) {
-  const response =
-    "Hello! What’s going on? Are you looking to create something interesting or collaborate? Feel free to contact me on the contact page, and I’ll get back to you as soon as possible. See yaa!";
-  res.status(200).json(response);
-}
