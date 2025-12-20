0ca718bda585e1967226b0db2c1e5c8417d1ead0
FthRobby <rbysalam10@gmail.com>
2025-12-20 18:19:24 +0700

    create new branch for migrating framework

--- src/pages/api/hello.js
+++
@@ -1,5 +0,0 @@
-export default function handler(req, res) {
-  const response = "Hello! What's going on? Are you looking to create something interesting or collaborate? Feel free to contact me on the contact page, and I’ll get back to you as soon as possible. See yaa!";
-  res.setHeader("Content-Type", "text/plain");
-  res.status(200).send(response);
-}
